e755c4c3f164228764b91b7677a9c69d38893bec
victorchiaka <chiakavictor205@gmail.com>
2023-08-16 12:49:00 +0100

    manage website design with tailwind

---
+++ tailwind.config.js
@@ -0,0 +1,12 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["*.html","./src/**/*.{html,js}"],
+  theme: {
+    extend: {
+      colors: {
+        coralOrange: '#f25c33',
+      }
+    },
+  },
+  plugins: [],
+}
